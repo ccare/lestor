1db94268cc13e0a1f0856cb5094ac6d53688bfda
beobal <sam.tunnicliffe@talis.com>
2012-02-10 19:45:43 +0000

    Tidied up BabuDb version
    Change-Id: I6621fb3c1d0c56e6a2b0996599a018bf6eee7ffa

--- src/main/java/com/talis/entity/db/babudb/BabuDbEntityDatabase.java
+++ src/main/java/com/talis/entity/db/babudb/BabuDbEntityDatabase.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Map.Entry;
+import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
@@ -17,6 +18,7 @@ import org.xtreemfs.babudb.api.database.Database;
 import org.xtreemfs.babudb.api.database.DatabaseInsertGroup;
 import org.xtreemfs.babudb.api.exception.BabuDBException;
 import org.xtreemfs.babudb.config.BabuDBConfig;
+import org.xtreemfs.babudb.config.ConfigBuilder;
 import org.xtreemfs.babudb.log.DiskLogger.SyncMode;
 import org.xtreemfs.babudb.tools.DBDumpTool;
 
@@ -34,17 +36,29 @@ public class BabuDbEntityDatabase implements EntityDatabase{
 
 	private static final Logger LOG = LoggerFactory.getLogger(BabuDbEntityDatabase.class);
 	
+	
 	public static final String DB_LOCATION_PROPERTY = "com.talis.entity.store.babudb.dir";
 	public static final String DB_DEFAULT_LOCATION = "/mnt/entity-cache/babudb";
+
+	private static Properties CONFIG_TEMPLATE;
+	static {
+		BabuDBConfig config = new ConfigBuilder()
+									.setCompressed(false)
+									.setMultiThreaded(0)
+									.setLogAppendSyncMode(SyncMode.ASYNC)
+									.build();
+		CONFIG_TEMPLATE = config.getProps();
+		CONFIG_TEMPLATE.setProperty("babudb.checkInterval", "30");
+	}
 	
 	private static final int SUBJECT_INDEX = 0;
 	private static final int GRAPH_INDEX = 1;
 	private static final int NUM_INDEXES = 2;
 	
-	public static final String DATA = "data";
+//	public static final String DATA = "data";
 	private final String dbName;
 	private File dbDir;
-	private File dataDir;
+//	private File dataDir;
 	
 	private final Serializer serializer;
 	private final VersionedDirectoryProvider directoryProvider;
@@ -58,16 +72,12 @@ public class BabuDbEntityDatabase implements EntityDatabase{
 		this.dbName = dbName;
 		initDatabase();
 	}
-
-	private void initDir(File dir){
-		LOG.info(String.format("Creating directory for DB at location : %s", dbDir.getAbsolutePath()));
-		try{
-			FileUtils.forceMkdir(dir);
-		}catch (Exception e){
-			String msg = String.format("Unable to create directory for DB at location: %s", dir.getAbsolutePath());
-			LOG.error(msg, e);
-			throw new RuntimeException(msg, e);
-		}
+	
+	public BabuDBConfig getConfiguration(File dbDir) throws IOException{
+		Properties props = new Properties(CONFIG_TEMPLATE);
+		props.setProperty("babudb.baseDir", dbDir.getAbsolutePath());
+		props.setProperty("babudb.logDir", dbDir.getAbsolutePath() + "/logs");
+		return new BabuDBConfig(props);
 	}
 	
 	protected void initDatabase(){
@@ -81,36 +91,10 @@ public class BabuDbEntityDatabase implements EntityDatabase{
     				throw new RuntimeException(msg);
     			}
     		}
-    		dataDir = new File(dbDir, DATA);
-    		initDir(dataDir);
-        	
-        	LOG.info("Initialising data store");
-        	String baseDir = dbDir.getAbsolutePath();
-        	String logDir = new File(dbDir, "logs").getAbsolutePath();
-        	int numThreads = 0;
-        	long maxLogFileSize = FileUtils.ONE_MB * 16;
-        	int checkInterval = 20;
-        	SyncMode syncMode = SyncMode.ASYNC;
-        	int pseudoSyncWait = 0;
-        	int maxQ = 0;
-        	boolean compression = false;
-        	int maxBlockSize = 64;
-        	int maxNumRecordsPerBlock = 52428800;
-        	boolean disableMMap = false;
-        	int mmapLimit = -1;
-        	int debugLevel = 4;
-        	
-        	BabuDBConfig config = new BabuDBConfig(baseDir, logDir, numThreads, maxLogFileSize,
-        											checkInterval, syncMode, pseudoSyncWait, 
-        											maxQ, compression, maxNumRecordsPerBlock, maxBlockSize,
-        											disableMMap, mmapLimit, debugLevel); 
-        	dbSystem = BabuDBFactory.createBabuDB(config);
-//        			new ConfigBuilder()
-//        				.setDataPath(dataDir.getAbsolutePath())
-//        				.setCompressed(false)
-//        				.setMultiThreaded(0)
-//        				.setLogAppendSyncMode(SyncMode.ASYNC)
-//        				.build());
+    		BabuDBConfig conf = getConfiguration(dbDir);
+    		System.out.println(conf.getCheckInterval());
+    		System.out.println(conf.getDbLogDir());
+        	dbSystem = BabuDBFactory.createBabuDB(getConfiguration(dbDir));
         	try{
         		db = dbSystem.getDatabaseManager().getDatabase("data");
         	}catch(BabuDBException e){
@@ -136,8 +120,8 @@ public class BabuDbEntityDatabase implements EntityDatabase{
 		byte[] inverseKey = getInverseKey(subject, graph);
 		try{
 			DatabaseInsertGroup batch = db.createInsertGroup();
-			batch.addInsert(0, storageKey, serializer.serialize(subject, graph, quads).bytes);
-			batch.addInsert(1, inverseKey, storageKey);
+			batch.addInsert(SUBJECT_INDEX, storageKey, serializer.serialize(subject, graph, quads).bytes);
+			batch.addInsert(GRAPH_INDEX, inverseKey, storageKey);
 			db.insert(batch, null);
 		} catch (Exception e) {
 			LOG.error("Unexpected exception writing to DB", e);
@@ -153,8 +137,8 @@ public class BabuDbEntityDatabase implements EntityDatabase{
 		byte[] inverseKey = getInverseKey(subject, graph);
 		try{
 			DatabaseInsertGroup batch = db.createInsertGroup();
-			batch.addInsert(0, storageKey, null);
-			batch.addInsert(1, inverseKey, null);
+			batch.addInsert(SUBJECT_INDEX, storageKey, null);
+			batch.addInsert(GRAPH_INDEX, inverseKey, null);
 			db.insert(batch, null);
 		} catch (Exception e) {
 			LOG.error("Unexpected exception writing to DB", e);
@@ -171,7 +155,7 @@ public class BabuDbEntityDatabase implements EntityDatabase{
 		EntityDesc desc = new EntityDesc();
 		desc.subject = subject;
 		try {
-			Iterator<Entry<byte[], byte[]>> iterator = db.prefixLookup(0, key, null).get();
+			Iterator<Entry<byte[], byte[]>> iterator = db.prefixLookup(SUBJECT_INDEX, key, null).get();
 			while(iterator.hasNext()) {
 			    Entry<byte[], byte[]> pair = iterator.next();
 			    String nextKey = asString(pair.getKey());
@@ -193,6 +177,59 @@ public class BabuDbEntityDatabase implements EntityDatabase{
 		return quads;
 	}
 
+	public Collection<Quad> getGraph(Node graph) throws EntityDatabaseException {
+		ArrayList<Quad> quads = new ArrayList<Quad>();
+		byte[] key = getKeyPrefix(graph);
+		EntityDesc desc = new EntityDesc();
+		desc.graph = graph;
+		try {
+		  Iterator<Entry<byte[], byte[]>> iterator = db.prefixLookup(GRAPH_INDEX, key, null).get();
+		  while (iterator.hasNext()){
+			  Entry<byte[], byte[]> pair = iterator.next();
+			  String nextKey = asString(pair.getKey());
+			  String[] keyParts = new String(nextKey).split("\t");
+			  if (! graph.getURI().equals(keyParts[0])){
+			    	continue;
+			  }
+			  desc.subject = Node.createURI(keyParts[1]);
+			  LOG.debug("Fetching single entity description");
+			  desc.bytes = db.lookup(SUBJECT_INDEX, pair.getValue(), null).get();
+			  LOG.debug("Fetched single entity description");
+			  quads.addAll(serializer.deserialize(desc));
+		  }
+		} catch (Exception e) {
+			LOG.error("Unexpected exception reading from DB", e);		
+			throw new EntityDatabaseException("Unexpected exception reading from DB", e);
+		}
+		return quads;
+	}
+	
+	public void clearGraph(Node graph) throws EntityDatabaseException {
+		byte[] key = getKeyPrefix(graph);
+		EntityDesc desc = new EntityDesc();
+		desc.graph = graph;
+		DatabaseInsertGroup batch = db.createInsertGroup();
+		try {
+		  Iterator<Entry<byte[], byte[]>> iterator = db.prefixLookup(GRAPH_INDEX, key, null).get();
+		  while (iterator.hasNext()){
+			  Entry<byte[], byte[]> pair = iterator.next();
+			  String nextKey = asString(pair.getKey());
+			  String[] keyParts = new String(nextKey).split("\t");
+			  if (! graph.getURI().equals(keyParts[0])){
+			    	continue;
+			  }
+			  desc.subject = Node.createURI(keyParts[1]);
+			  batch.addDelete(SUBJECT_INDEX, pair.getValue());
+			  batch.addDelete(GRAPH_INDEX, pair.getKey());
+		  }
+		  db.insert(batch, null);
+		} catch (Exception e) {
+			LOG.error("Unexpected exception reading from DB", e);		
+			throw new EntityDatabaseException("Unexpected exception reading from DB", e);
+		}
+	}
+	
+	
 	@Override
 	public void clear() throws EntityDatabaseException {
 		File currentLiveDirectory = null;
@@ -228,59 +265,6 @@ public class BabuDbEntityDatabase implements EntityDatabase{
 		}
 		initDatabase();
 	}
-	
-	public Collection<Quad> getGraph(Node graph) throws EntityDatabaseException {
-		ArrayList<Quad> quads = new ArrayList<Quad>();
-		byte[] key = getKeyPrefix(graph);
-		EntityDesc desc = new EntityDesc();
-		desc.graph = graph;
-		try {
-		  Iterator<Entry<byte[], byte[]>> iterator = db.prefixLookup(1, key, null).get();
-		  while (iterator.hasNext()){
-			  Entry<byte[], byte[]> pair = iterator.next();
-			  String nextKey = asString(pair.getKey());
-			  String[] keyParts = new String(nextKey).split("\t");
-			  if (! graph.getURI().equals(keyParts[0])){
-			    	continue;
-			  }
-			  desc.subject = Node.createURI(keyParts[1]);
-			  LOG.debug("Fetching single entity description");
-			  desc.bytes = db.lookup(0, pair.getValue(), null).get();
-			  LOG.debug("Fetched single entity description");
-			  quads.addAll(serializer.deserialize(desc));
-		  }
-		} catch (Exception e) {
-			LOG.error("Unexpected exception reading from DB", e);		
-			throw new EntityDatabaseException("Unexpected exception reading from DB", e);
-		}
-		return quads;
-	}
-	
-	public void clearGraph(Node graph) throws EntityDatabaseException {
-		byte[] key = getKeyPrefix(graph);
-		EntityDesc desc = new EntityDesc();
-		desc.graph = graph;
-		DatabaseInsertGroup batch = db.createInsertGroup();
-		try {
-		  Iterator<Entry<byte[], byte[]>> iterator = db.prefixLookup(1, key, null).get();
-		  while (iterator.hasNext()){
-			  Entry<byte[], byte[]> pair = iterator.next();
-			  String nextKey = asString(pair.getKey());
-			  String[] keyParts = new String(nextKey).split("\t");
-			  if (! graph.getURI().equals(keyParts[0])){
-			    	continue;
-			  }
-			  desc.subject = Node.createURI(keyParts[1]);
-			  batch.addDelete(0, pair.getValue());
-			  batch.addDelete(1, pair.getKey());
-		  }
-		  db.insert(batch, null);
-		} catch (Exception e) {
-			LOG.error("Unexpected exception reading from DB", e);		
-			throw new EntityDatabaseException("Unexpected exception reading from DB", e);
-		}
-	}
-	
 
 	@Override
 	public void begin() throws EntityDatabaseException {
